d79abf40b3a51d5a4b0bc32743b83ad46bd5163c
Sylvain HENRY <sylvain.henry@inria.fr>
2010-08-30 20:27:22 +0200

    Implement StarPU events for CUDA

---
+++ src/drivers/cuda/driver_cuda_event.h
@@ -0,0 +1,26 @@
+/*
+ * StarPU
+ * Copyright (C) Université Bordeaux 1, CNRS 2008-2010 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+#ifndef __DRIVER_CUDA_EVENT_H__
+#define __DRIVER_CUDA_EVENT_H__
+
+#ifdef STARPU_USE_CUDA
+
+#include <cuda.h>
+
+starpu_event _starpu_cuda_event_create(cudaEvent_t);
+
+#endif // __DRIVER_CUDA_EVENT_H__
