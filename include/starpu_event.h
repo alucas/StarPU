9d7223f6aab628d404326df0b10ed30f2c2d1e7d
Sylvain HENRY <sylvain.henry@inria.fr>
2010-08-30 18:12:58 +0200

    Add starpu_event.h header

---
+++ include/starpu_event.h
@@ -0,0 +1,43 @@
+/*
+ * StarPU
+ * Copyright (C) Université Bordeaux 1, CNRS 2008-2010 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+#ifndef __STARPU_EVENT_H__
+#define __STARPU_EVENT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct starpu_event_t * starpu_event;
+
+int starpu_event_release(starpu_event event);
+int starpu_event_retain(starpu_event event);
+
+int starpu_events_wait(int num_events, starpu_event *events);
+
+
+enum starpu_event_info_t {
+   STARPU_EVENT_EXECUTION_STATUS
+};
+
+int starpu_event_info(starpu_event event, enum starpu_event_info_t param_name, void *ret);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __STARPU_EVENT_H__
