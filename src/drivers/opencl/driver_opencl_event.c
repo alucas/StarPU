1e3ff489db3ef063ca4e548c9a9fd446af54d7fc
Sylvain HENRY <sylvain.henry@inria.fr>
2010-08-30 23:47:15 +0200

    CUDA and OpenCL event implementations now use generic event implementation.
    Triggers can be registered on events.

--- src/drivers/opencl/driver_opencl_event.c
+++ src/drivers/opencl/driver_opencl_event.c
@@ -21,52 +21,21 @@
 #include <core/event.h>
 #include "driver_opencl_event.h"
 
-#define CLCHECK(f) assert(CL_SUCCESS == (f));
+static void opencl_event_callback(cl_event event, cl_int status, void *user_data) {
+   starpu_event ev = (starpu_event)user_data;
 
-static int _starpu_opencl_event_init(starpu_event);
-static int _starpu_opencl_event_wait(starpu_event);
-static int _starpu_opencl_event_free(starpu_event);
-static starpu_event_status_t _starpu_opencl_event_status(starpu_event);
+   _starpu_event_complete(ev);
 
-static struct starpu_event_methods_t methods = {
-   .wait = _starpu_opencl_event_wait,
-   .free = _starpu_opencl_event_free,
-   .status = _starpu_opencl_event_status
-};
+   clReleaseEvent(event);
+}
 
 starpu_event _starpu_opencl_event_create(cl_event event) {
-   return _starpu_event_create(&methods, event);
+
+   starpu_event ev = _starpu_event_create();
+
+   clSetEventCallback(event, CL_COMPLETE, opencl_event_callback, ev);
+
+   return ev;
 }
-
-static int _starpu_opencl_event_wait(starpu_event event) {
-   cl_event ev = (cl_event)_starpu_event_data(event);
-   CLCHECK((err = clWaitForEvents(1, &ev)));
-
-   return (err == CL_SUCCESS ? 0 : 1);
-}
-
-static int _starpu_opencl_event_free(starpu_event event) {
-   cl_event ev = (cl_event)_starpu_event_data(event);
-   CLCHECK((err = clReleaseEvent(ev)));
-
-   return (err == CL_SUCCESS ? 0 : 1);
-}
-
-static starpu_event_status_t _starpu_opencl_event_status(starpu_event event) {
-   cl_event ev = (cl_event)_starpu_event_data(event);
-   cl_int status;
-   CLCHECK(clGetEventInfo(ev, Cl_EVENT_COMMAND_EXECUTION_STATUS, sizeof(cl_int), &status, NULL));
-   switch (status) {
-      case CL_QUEUED:
-      case CL_SUBMITTED:
-         return STARPU_EVENT_WAITING;
-      case CL_RUNNING:
-         return STARPU_EVENT_RUNNING;
-      case CL_COMPLETE:
-         return STARPU_EVENT_COMPLETE;
-   }
-}
-
-#undef CLCHECK
 
 #endif // STARPU_USE_OPENCL
