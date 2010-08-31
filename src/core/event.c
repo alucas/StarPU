ad2be46e98ce5bf9509fb8962890fb9a3c6255c2
Sylvain HENRY <sylvain.henry@inria.fr>
2010-08-31 19:27:09 +0200

    Fix bugs with events

--- src/core/event.c
+++ src/core/event.c
@@ -126,6 +126,7 @@ starpu_event starpu_event_create() {
    event->user_event = 1;
    event->ref_count = 1;
    event->ref_count_priv = 0;
+   return event;
 }
 
 void starpu_event_trigger(starpu_event event) {
