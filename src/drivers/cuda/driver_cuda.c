011d3c18535e2e65115782ac7c6355e11ea445f5
Sylvain HENRY <sylvain.henry@inria.fr>
2010-09-02 18:03:04 +0200

    Indentation

--- src/drivers/cuda/driver_cuda.c
+++ src/drivers/cuda/driver_cuda.c
@@ -33,240 +33,240 @@ static cudaStream_t streams[STARPU_NMAXWORKERS];
 
 cudaStream_t *starpu_cuda_get_local_stream(void)
 {
-	int worker = starpu_worker_get_id();
+   int worker = starpu_worker_get_id();
 
-	return &streams[worker];
+   return &streams[worker];
 }
 
 static void init_context(int devid)
 {
-	cudaError_t cures;
+   cudaError_t cures;
 
-	cures = cudaSetDevice(devid);
-	if (STARPU_UNLIKELY(cures))
-		STARPU_CUDA_REPORT_ERROR(cures);
+   cures = cudaSetDevice(devid);
+   if (STARPU_UNLIKELY(cures))
+      STARPU_CUDA_REPORT_ERROR(cures);
 
-	/* force CUDA to initialize the context for real */
-	cudaFree(0);
+   /* force CUDA to initialize the context for real */
+   cudaFree(0);
 
-	cures = cudaStreamCreate(starpu_cuda_get_local_stream());
-	if (STARPU_UNLIKELY(cures))
-		STARPU_CUDA_REPORT_ERROR(cures);
+   cures = cudaStreamCreate(starpu_cuda_get_local_stream());
+   if (STARPU_UNLIKELY(cures))
+      STARPU_CUDA_REPORT_ERROR(cures);
 }
 
 static void deinit_context(int workerid)
 {
-	cudaError_t cures;
+   cudaError_t cures;
 
-	cudaStreamDestroy(streams[workerid]);
+   cudaStreamDestroy(streams[workerid]);
 
-	/* cleanup the runtime API internal stuffs (which CUBLAS is using) */
-	cures = cudaThreadExit();
-	if (cures)
-		STARPU_CUDA_REPORT_ERROR(cures);
+   /* cleanup the runtime API internal stuffs (which CUBLAS is using) */
+   cures = cudaThreadExit();
+   if (cures)
+      STARPU_CUDA_REPORT_ERROR(cures);
 }
 
 unsigned _starpu_get_cuda_device_count(void)
 {
-	int cnt;
+   int cnt;
 
-	cudaError_t cures;
-	cures = cudaGetDeviceCount(&cnt);
-	if (STARPU_UNLIKELY(cures))
-		 return 0;
-	
-	return (unsigned)cnt;
+   cudaError_t cures;
+   cures = cudaGetDeviceCount(&cnt);
+   if (STARPU_UNLIKELY(cures))
+      return 0;
+
+   return (unsigned)cnt;
 }
 
 void _starpu_init_cuda(void)
 {
-	ncudagpus = _starpu_get_cuda_device_count();
-	assert(ncudagpus <= STARPU_MAXCUDADEVS);
+   ncudagpus = _starpu_get_cuda_device_count();
+   assert(ncudagpus <= STARPU_MAXCUDADEVS);
 }
 
 static int execute_job_on_cuda(starpu_job_t j, struct starpu_worker_s *args)
 {
-	int ret;
-	uint32_t mask = 0;
+   int ret;
+   uint32_t mask = 0;
 
-	STARPU_ASSERT(j);
-	struct starpu_task *task = j->task;
+   STARPU_ASSERT(j);
+   struct starpu_task *task = j->task;
 
-	struct timespec codelet_start, codelet_end;
+   struct timespec codelet_start, codelet_end;
 
-	unsigned calibrate_model = 0;
-	int workerid = args->workerid;
+   unsigned calibrate_model = 0;
+   int workerid = args->workerid;
 
-	STARPU_ASSERT(task);
-	struct starpu_codelet_t *cl = task->cl;
-	STARPU_ASSERT(cl);
+   STARPU_ASSERT(task);
+   struct starpu_codelet_t *cl = task->cl;
+   STARPU_ASSERT(cl);
 
-	if (cl->model && cl->model->benchmarking) 
-		calibrate_model = 1;
+   if (cl->model && cl->model->benchmarking) 
+      calibrate_model = 1;
 
-	ret = _starpu_fetch_task_input(task, mask);
-	if (ret != 0) {
-		/* there was not enough memory, so the input of
-		 * the codelet cannot be fetched ... put the 
-		 * codelet back, and try it later */
-		return -EAGAIN;
-	}
+   ret = _starpu_fetch_task_input(task, mask);
+   if (ret != 0) {
+      /* there was not enough memory, so the input of
+       * the codelet cannot be fetched ... put the 
+       * codelet back, and try it later */
+      return -EAGAIN;
+   }
 
-	STARPU_TRACE_START_CODELET_BODY(j);
+   STARPU_TRACE_START_CODELET_BODY(j);
 
    int event_prof = starpu_event_profiling_enabled(j->event);
 
-	if (event_prof || calibrate_model)
-	{
-		starpu_clock_gettime(&codelet_start);
-		_starpu_worker_register_executing_start_date(workerid, &codelet_start);
-	}
+   if (event_prof || calibrate_model)
+   {
+      starpu_clock_gettime(&codelet_start);
+      _starpu_worker_register_executing_start_date(workerid, &codelet_start);
+   }
 
-	args->status = STATUS_EXECUTING;
-	task->status = STARPU_TASK_RUNNING;	
+   args->status = STATUS_EXECUTING;
+   task->status = STARPU_TASK_RUNNING;	
 
-	cl_func func = cl->cuda_func;
-	STARPU_ASSERT(func);
-	func(task->interface, task->cl_arg);
+   cl_func func = cl->cuda_func;
+   STARPU_ASSERT(func);
+   func(task->interface, task->cl_arg);
 
-	cl->per_worker_stats[workerid]++;
+   cl->per_worker_stats[workerid]++;
 
-	if (event_prof || calibrate_model)
-		starpu_clock_gettime(&codelet_end);
+   if (event_prof || calibrate_model)
+      starpu_clock_gettime(&codelet_end);
 
-	STARPU_TRACE_END_CODELET_BODY(j);	
-	args->status = STATUS_UNKNOWN;
+   STARPU_TRACE_END_CODELET_BODY(j);	
+   args->status = STATUS_UNKNOWN;
 
-	_starpu_push_task_output(task, mask);
+   _starpu_push_task_output(task, mask);
 
-	_starpu_driver_update_job_feedback(j, args, calibrate_model, &codelet_start, &codelet_end);
+   _starpu_driver_update_job_feedback(j, args, calibrate_model, &codelet_start, &codelet_end);
 
-	return 0;
+   return 0;
 }
 
 void *_starpu_cuda_worker(void *arg)
 {
-	struct starpu_worker_s* args = arg;
+   struct starpu_worker_s* args = arg;
 
-	int devid = args->devid;
-	int workerid = args->workerid;
-	unsigned memnode = args->memory_node;
+   int devid = args->devid;
+   int workerid = args->workerid;
+   unsigned memnode = args->memory_node;
 
 #ifdef STARPU_USE_FXT
-	_starpu_fxt_register_thread(args->bindid);
+   _starpu_fxt_register_thread(args->bindid);
 #endif
-	STARPU_TRACE_WORKER_INIT_START(STARPU_FUT_CUDA_KEY, devid, memnode);
+   STARPU_TRACE_WORKER_INIT_START(STARPU_FUT_CUDA_KEY, devid, memnode);
 
-	_starpu_bind_thread_on_cpu(args->config, args->bindid);
+   _starpu_bind_thread_on_cpu(args->config, args->bindid);
 
-	_starpu_set_local_memory_node_key(&memnode);
+   _starpu_set_local_memory_node_key(&memnode);
 
-	_starpu_set_local_worker_key(args);
+   _starpu_set_local_worker_key(args);
 
-	init_context(devid);
+   init_context(devid);
 
-	/* one more time to avoid hacks from third party lib :) */
-	_starpu_bind_thread_on_cpu(args->config, args->bindid);
+   /* one more time to avoid hacks from third party lib :) */
+   _starpu_bind_thread_on_cpu(args->config, args->bindid);
 
-	args->status = STATUS_UNKNOWN;
+   args->status = STATUS_UNKNOWN;
 
-	/* get the device's name */
-	char devname[128];
-	struct cudaDeviceProp prop;
-	cudaGetDeviceProperties(&prop, devid);
-	strncpy(devname, prop.name, 128);
-	snprintf(args->name, 32, "CUDA %d (%s)", args->devid, devname);
+   /* get the device's name */
+   char devname[128];
+   struct cudaDeviceProp prop;
+   cudaGetDeviceProperties(&prop, devid);
+   strncpy(devname, prop.name, 128);
+   snprintf(args->name, 32, "CUDA %d (%s)", args->devid, devname);
 
 #ifdef STARPU_VERBOSE
-	fprintf(stderr, "cuda (%s) dev id %d thread is ready to run on CPU %d !\n", devname, devid, args->bindid);
+   fprintf(stderr, "cuda (%s) dev id %d thread is ready to run on CPU %d !\n", devname, devid, args->bindid);
 #endif
 
-	STARPU_TRACE_WORKER_INIT_END
+   STARPU_TRACE_WORKER_INIT_END
 
-	/* tell the main thread that this one is ready */
-	PTHREAD_MUTEX_LOCK(&args->mutex);
-	args->worker_is_initialized = 1;
-	PTHREAD_COND_SIGNAL(&args->ready_cond);
-	PTHREAD_MUTEX_UNLOCK(&args->mutex);
+      /* tell the main thread that this one is ready */
+      PTHREAD_MUTEX_LOCK(&args->mutex);
+   args->worker_is_initialized = 1;
+   PTHREAD_COND_SIGNAL(&args->ready_cond);
+   PTHREAD_MUTEX_UNLOCK(&args->mutex);
 
-	struct starpu_job_s * j;
-	int res;
+   struct starpu_job_s * j;
+   int res;
 
-	while (_starpu_machine_is_running())
-	{
-		STARPU_TRACE_START_PROGRESS(memnode);
-		_starpu_datawizard_progress(memnode, 1);
-		STARPU_TRACE_END_PROGRESS(memnode);
+   while (_starpu_machine_is_running())
+   {
+      STARPU_TRACE_START_PROGRESS(memnode);
+      _starpu_datawizard_progress(memnode, 1);
+      STARPU_TRACE_END_PROGRESS(memnode);
 
-		_starpu_execute_registered_progression_hooks();
-	
-		PTHREAD_MUTEX_LOCK(args->sched_mutex);
+      _starpu_execute_registered_progression_hooks();
 
-		/* perhaps there is some local task to be executed first */
-		j = _starpu_pop_local_task(args);
+      PTHREAD_MUTEX_LOCK(args->sched_mutex);
 
-		/* otherwise ask a task to the scheduler */
-		if (!j)
-		{
-			struct starpu_task *task = _starpu_pop_task();
-			if (task)
-				j = _starpu_get_job_associated_to_task(task);
-		}
-	
-                if (j == NULL) 
-		{
-			if (_starpu_worker_can_block(memnode))
-				_starpu_block_worker(workerid, args->sched_cond, args->sched_mutex);
+      /* perhaps there is some local task to be executed first */
+      j = _starpu_pop_local_task(args);
 
-			PTHREAD_MUTEX_UNLOCK(args->sched_mutex);
+      /* otherwise ask a task to the scheduler */
+      if (!j)
+      {
+         struct starpu_task *task = _starpu_pop_task();
+         if (task)
+            j = _starpu_get_job_associated_to_task(task);
+      }
 
-			continue;
-		};
+      if (j == NULL) 
+      {
+         if (_starpu_worker_can_block(memnode))
+            _starpu_block_worker(workerid, args->sched_cond, args->sched_mutex);
 
-		PTHREAD_MUTEX_UNLOCK(args->sched_mutex);
+         PTHREAD_MUTEX_UNLOCK(args->sched_mutex);
 
-		/* can CUDA do that task ? */
-		if (!STARPU_CUDA_MAY_PERFORM(j))
-		{
-			/* this is neither a cuda or a cublas task */
-			_starpu_push_task(j, 0);
-			continue;
-		}
+         continue;
+      };
 
-		_starpu_set_current_task(j->task);
+      PTHREAD_MUTEX_UNLOCK(args->sched_mutex);
 
-		res = execute_job_on_cuda(j, args);
+      /* can CUDA do that task ? */
+      if (!STARPU_CUDA_MAY_PERFORM(j))
+      {
+         /* this is neither a cuda or a cublas task */
+         _starpu_push_task(j, 0);
+         continue;
+      }
 
-		_starpu_set_current_task(NULL);
+      _starpu_set_current_task(j->task);
 
-		if (res) {
-			switch (res) {
-				case -EAGAIN:
-					fprintf(stderr, "ouch, put the codelet %p back ... \n", j);
-					_starpu_push_task(j, 0);
-					STARPU_ABORT();
-					continue;
-				default:
-					assert(0);
-			}
-		}
+      res = execute_job_on_cuda(j, args);
 
-		_starpu_handle_job_termination(j, 0);
-	}
+      _starpu_set_current_task(NULL);
 
-	STARPU_TRACE_WORKER_DEINIT_START
+      if (res) {
+         switch (res) {
+            case -EAGAIN:
+               fprintf(stderr, "ouch, put the codelet %p back ... \n", j);
+               _starpu_push_task(j, 0);
+               STARPU_ABORT();
+               continue;
+            default:
+               assert(0);
+         }
+      }
 
-	/* In case there remains some memory that was automatically
-	 * allocated by StarPU, we release it now. Note that data
-	 * coherency is not maintained anymore at that point ! */
-	_starpu_free_all_automatically_allocated_buffers(memnode);
+      _starpu_handle_job_termination(j, 0);
+   }
 
-	deinit_context(args->workerid);
+   STARPU_TRACE_WORKER_DEINIT_START
 
-	STARPU_TRACE_WORKER_DEINIT_END(STARPU_FUT_CUDA_KEY);
+      /* In case there remains some memory that was automatically
+       * allocated by StarPU, we release it now. Note that data
+       * coherency is not maintained anymore at that point ! */
+      _starpu_free_all_automatically_allocated_buffers(memnode);
 
-	pthread_exit(NULL);
+   deinit_context(args->workerid);
 
-	return NULL;
+   STARPU_TRACE_WORKER_DEINIT_END(STARPU_FUT_CUDA_KEY);
+
+   pthread_exit(NULL);
+
+   return NULL;
 
 }
--- src/drivers/driver_common/driver_common.c
+++ src/drivers/driver_common/driver_common.c
@@ -23,59 +23,59 @@
 #include <core/event.h>
 
 void _starpu_driver_update_job_feedback(starpu_job_t j, struct starpu_worker_s *worker_args,
-					unsigned calibrate_model,
-					struct timespec *codelet_start, struct timespec *codelet_end)
+      unsigned calibrate_model,
+      struct timespec *codelet_start, struct timespec *codelet_end)
 {
-	struct timespec measured_ts;
-	double measured;
+   struct timespec measured_ts;
+   double measured;
 
    int event_prof = starpu_event_profiling_enabled(j->event);
 
-	if (event_prof || calibrate_model)
-	{
-		starpu_timespec_sub(codelet_end, codelet_start, &measured_ts);
-		measured = starpu_timing_timespec_to_us(&measured_ts);
+   if (event_prof || calibrate_model)
+   {
+      starpu_timespec_sub(codelet_end, codelet_start, &measured_ts);
+      measured = starpu_timing_timespec_to_us(&measured_ts);
 
-		if (event_prof)
-		{
+      if (event_prof)
+      {
          _starpu_event_profiling_start_time_set(j->event, codelet_start);
          _starpu_event_profiling_end_time_set(j->event, codelet_end);
 
-			int workerid = worker_args->workerid;
+         int workerid = worker_args->workerid;
 
          _starpu_event_profiling_worker_id_set(j->event, workerid);
-			
-			_starpu_worker_update_profiling_info_executing(workerid, &measured_ts, 1);
-		}
 
-		if (calibrate_model)
-			_starpu_update_perfmodel_history(j, worker_args->perf_arch, worker_args->devid, measured);
-	}
+         _starpu_worker_update_profiling_info_executing(workerid, &measured_ts, 1);
+      }
+
+      if (calibrate_model)
+         _starpu_update_perfmodel_history(j, worker_args->perf_arch, worker_args->devid, measured);
+   }
 }
 
 /* Workers may block when there is no work to do at all. We assume that the
  * mutex is hold when that function is called. */
 void _starpu_block_worker(int workerid, pthread_cond_t *cond, pthread_mutex_t *mutex)
 {
-	struct timespec start_time, end_time;
+   struct timespec start_time, end_time;
 
-	STARPU_TRACE_WORKER_SLEEP_START
-	_starpu_worker_set_status(workerid, STATUS_SLEEPING);
+   STARPU_TRACE_WORKER_SLEEP_START
+      _starpu_worker_set_status(workerid, STATUS_SLEEPING);
 
-	starpu_clock_gettime(&start_time);
-	_starpu_worker_register_sleeping_start_date(workerid, &start_time);
+   starpu_clock_gettime(&start_time);
+   _starpu_worker_register_sleeping_start_date(workerid, &start_time);
 
-	PTHREAD_COND_WAIT(cond, mutex);
+   PTHREAD_COND_WAIT(cond, mutex);
 
-	_starpu_worker_set_status(workerid, STATUS_UNKNOWN);
-	STARPU_TRACE_WORKER_SLEEP_END
-	starpu_clock_gettime(&end_time);
+   _starpu_worker_set_status(workerid, STATUS_UNKNOWN);
+   STARPU_TRACE_WORKER_SLEEP_END
+      starpu_clock_gettime(&end_time);
 
-	int profiling = starpu_profiling_status_get();
-	if (profiling)
-	{
-		struct timespec sleeping_time;
-		starpu_timespec_sub(&end_time, &start_time, &sleeping_time);
-		_starpu_worker_update_profiling_info_sleeping(workerid, &start_time, &end_time);
-	}
+   int profiling = starpu_profiling_status_get();
+   if (profiling)
+   {
+      struct timespec sleeping_time;
+      starpu_timespec_sub(&end_time, &start_time, &sleeping_time);
+      _starpu_worker_update_profiling_info_sleeping(workerid, &start_time, &end_time);
+   }
 }
