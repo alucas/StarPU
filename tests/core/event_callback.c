71a5c19738b5529b0a666c911f2a5aba1f233d44
Sylvain HENRY <sylvain.henry@inria.fr>
2010-09-03 14:37:51 +0200

    Add event callback

---
+++ tests/core/event_callback.c
@@ -0,0 +1,46 @@
+/*
+ * StarPU
+ * Copyright (C) Université Bordeaux 1, CNRS 2008-2010 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+#include <starpu.h>
+#include <unistd.h>
+
+void callback(void*);
+
+int main() {
+
+   starpu_event event;
+   int val = 0;
+   unsigned count = 10;
+
+   event = starpu_event_create();
+
+   starpu_event_callback_add(event, &callback, &val);
+
+   starpu_event_trigger(event);
+   starpu_event_release(event);
+
+   while ((volatile int)val != 999 && count != 0) {
+      count -= 1;
+      usleep(50);
+   }
+
+   return (val != 999 ? -1 : 0);
+}
+
+void callback(void*data) {
+   int * val = (int*)data;
+   *val = 999;
+}
